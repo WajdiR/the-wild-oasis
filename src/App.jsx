e84b1c7e6570137c6bd67185d6e13b334a54c5ba
WAJDI RIAHI <rwajdi2020@gmail.com>
2023-09-20 01:02:30 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { styled } from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  color: blue;
+`;
+
 function App() {
-  return <div>Hello world!</div>;
+  return (
+    <div>
+      <H1>Helloo world! </H1>{" "}
+    </div>
+  );
 }
 
 export default App;
