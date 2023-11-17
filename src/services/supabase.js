1347b9535899ae27c26132864e4de5b23531b2fe
WAJDI RIAHI <rwajdi2020@gmail.com>
2023-11-17 20:38:33 +0100

    Supabase adding

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://dclaevazetcjjkrzczpc.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5hbmlrdmhjbmJyZWJwb3p1c2ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTUzMzUwMzEsImV4cCI6MjAxMDkxMTAzMX0.vsvKsACsu1DiFy6tVPfc5sNp7XCFf0hfadXZ0JhEIDQ";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
